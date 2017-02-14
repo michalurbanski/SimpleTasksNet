3fcc51642e6fdb5a5a2038a748dc6559d5624f98
michal_urbanski <michal_urbanski@windowslive.com>
2017-02-14 21:29:04 +0100

    Adds first unit test to confirm if nunit works correctly

--- tests/SimpleTasksNet.Business.Tests/Class1.cs
+++ tests/SimpleTasksNet.Business.Tests/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace SimpleTasksNet.Business.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void test_nunit()
+        {
+            Assert.True(true);
+        }
     }
 }
