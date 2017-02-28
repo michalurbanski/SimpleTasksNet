66aab3211bd5510aa598cf163ffec36101aba0a1
michal_urbanski <michal_urbanski@windowslive.com>
2017-02-28 19:22:49 +0100

    Logger usage

--- src/SimpleTasksNet.Console/Program.cs
+++ src/SimpleTasksNet.Console/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SimpleTasksNet.Logging.Implementation;
+using SimpleTasksNet.Logging.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,11 @@ namespace SimpleTasksNet.Console
 {
     class Program
     {
+        private static readonly ILogger _logger = LogManager.GetLogger(typeof(Program)); 
+
         static void Main(string[] args)
         {
+            _logger.Info("Progam started");
         }
     }
 }
