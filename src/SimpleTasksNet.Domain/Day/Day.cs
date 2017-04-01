c4f8d5f304f5a44af23602bd17a6e9a593752a1d
michal_urbanski <michal_urbanski@windowslive.com>
2017-04-01 16:44:20 +0200

    Improves Day class

--- src/SimpleTasksNet.Domain/Day/Day.cs
+++ src/SimpleTasksNet.Domain/Day/Day.cs
@@ -4,25 +4,20 @@ namespace SimpleTasksNet.Domain
 {
     public class Day
     {
-        private readonly List<CustomTask> _tasks;
+        public List<CustomTask> Tasks { get; private set;}
 
         public string Title { get; private set; }
-        public int TasksCount { get { return _tasks.Count; } }
-
-        public IEnumerable<CustomTask> Tasks
-        {
-            get { return _tasks; }
-        }
+        public int TasksCount { get { return Tasks.Count; } }
 
         public Day(string title)
         {
             Title = title;
-            _tasks = new List<CustomTask>(); 
+            Tasks = new List<CustomTask>(); 
         }
 
         public void AddTask(CustomTask customTask)
         {
-            _tasks.Add(customTask); 
+            Tasks.Add(customTask); 
         }
     }
 }
