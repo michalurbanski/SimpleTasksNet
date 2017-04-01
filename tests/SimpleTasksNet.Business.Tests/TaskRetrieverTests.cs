29d2acb7ef3bdb11365499d38758213665f20141
michal_urbanski <michal_urbanski@windowslive.com>
2017-04-01 16:54:11 +0200

    Tests for task retriever

--- tests/SimpleTasksNet.Business.Tests/TaskRetrieverTests.cs
+++ tests/SimpleTasksNet.Business.Tests/TaskRetrieverTests.cs
@@ -1,10 +1,7 @@
 ﻿using NUnit.Framework;
 using SimpleTasksNet.Domain;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleTasksNet.Business.Tests
 {
@@ -16,26 +13,63 @@ namespace SimpleTasksNet.Business.Tests
         {
             Week week = new Week("Week 1");
 
-            TaskRetriever taskRetriever = new TaskRetriever();
-            IEnumerable<CustomTask> tasks = taskRetriever.GetTasks(week);
+            var tasks = act(week); 
+            
+            Assert.That(!tasks.Any());
         }
 
         [Test]
         public void test_week_with_days_with_no_tasks()
         {
-            Assert.Inconclusive(); 
+            Week week = new Week("Week 1");
+            Day day = new Day("Monday 2017-04-03");
+            week.AddDay(day);
+
+            var tasks = act(week);
+
+            Assert.That(!tasks.Any());
         }
 
         [Test]
         public void test_retrieve_tasks_from_week_single_day()
         {
-            Assert.Inconclusive(); 
+            Week week = new Week("Week 1");
+            Day day = new Day("Monday 2017-04-03");
+            day.AddTask(new CustomTask("- First task"));
+            day.AddTask(new CustomTask("- Second task"));
+
+            week.AddDay(day);
+
+            var tasks = act(week);
+
+            Assert.That(tasks.Count() == 2);
         }
 
         [Test]
         public void test_retrieve_tasks_from_week_multiple_days()
         {
-            Assert.Inconclusive(); 
+            Week week = new Week("Week 1");
+            Day monday = new Day("Monday 2017-04-03");
+            monday.AddTask(new CustomTask("- First task"));
+            monday.AddTask(new CustomTask("- Second task"));
+
+            Day tuesday = new Day("Tuesday 2017-04-04");
+            tuesday.AddTask(new CustomTask("- Third task"));
+            tuesday.AddTask(new CustomTask("- Fourth task")); 
+
+            week.AddDay(monday);
+            week.AddDay(tuesday); 
+
+            var tasks = act(week);
+
+            Assert.That(tasks.Count() == 4);
+        }
+
+        private IEnumerable<CustomTask> act(Week week)
+        {
+            TaskRetriever taskRetriever = new TaskRetriever();
+
+            return taskRetriever.GetTasks(week);
         }
     }
 }
