89ea09ee874da00992982f39087f252deb4dc367
michal_urbanski <michal_urbanski@windowslive.com>
2017-04-01 16:40:17 +0200

    Allows to get all tasks from week

---
+++ src/SimpleTasksNet.Business/TaskRetriever.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SimpleTasksNet.Domain;
+
+namespace SimpleTasksNet.Business
+{
+    public class TaskRetriever
+    {
+        public IEnumerable<CustomTask> GetTasks(Week week)
+        {
+            var days = week.Days;
+
+            return days.SelectMany(s => s.Tasks);
+        }
+    }
+}
---
+++ tests/SimpleTasksNet.Business.Tests/TaskRetrieverTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using SimpleTasksNet.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTasksNet.Business.Tests
+{
+    [TestFixture]
+    class TaskRetrieverTests
+    {
+        [Test]
+        public void test_week_without_days_has_no_tasks()
+        {
+            Week week = new Week("Week 1");
+
+            TaskRetriever taskRetriever = new TaskRetriever();
+            IEnumerable<CustomTask> tasks = taskRetriever.GetTasks(week);
+        }
+
+        [Test]
+        public void test_week_with_days_with_no_tasks()
+        {
+            Assert.Inconclusive(); 
+        }
+
+        [Test]
+        public void test_retrieve_tasks_from_week_single_day()
+        {
+            Assert.Inconclusive(); 
+        }
+
+        [Test]
+        public void test_retrieve_tasks_from_week_multiple_days()
+        {
+            Assert.Inconclusive(); 
+        }
+    }
+}
